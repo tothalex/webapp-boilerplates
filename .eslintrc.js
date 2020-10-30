81cb195d6e9ebe5aaf1929e4d0cb6045a93a51d5
Toth Alex <tothalex@Toths-MacBook-Pro.local>
2020-10-30 23:42:58 +0100

    issues fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,43 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true
+  env: {
+    browser: true,
+    es6: true
   },
-  "extends": [
-      "plugin:react/recommended",
-      "standard",
-      "plugin:prettier/recommended",
-      "plugin:@typescript-eslint/recommended"
-  ],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly",
-      "React": "writable"
+  extends: ['plugin:react/recommended', 'standard', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    React: 'writable'
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  "plugins": [
-      "react",
-      "@typescript-eslint"
-  ],
-  "rules": {
-    "new-cap": "off",
-    "no-useless-constructor": "off",
-    "@typescript-eslint/no-useless-constructor": ["error"],
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/member-delimiter-style": ["error", {
-      "multiline": {
-          "delimiter": "none",
-          "requireLast": true
-      },
-      "singleline": {
-          "delimiter": "semi",
-          "requireLast": false
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'new-cap': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
+        }
       }
-  }],
+    ]
   }
-};
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const { PORT = 3000 } = process.env
 
   const app = await NestFactory.create(AppModule)
